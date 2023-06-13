1859b9cda7d7f5ca12c7786e002d27b4419e9197
David Ihl <daih1985@gmail.com>
2023-06-13 14:05:09 +0200

    Create folders and fakeDB for graphQL setup

---
+++ database/database.ts
@@ -0,0 +1,101 @@
+export type User = {
+  id: number;
+  password: string;
+  is_admin: boolean;
+  is_player: boolean;
+  created: string;
+  last_update: string;
+};
+
+export const user = [
+  {
+    id: 1,
+    username: 'callcott0',
+    password: '$2a$04$Ow4g6MF3PuSLreeRlJ0OyeTTst.XTDIAxxQ8opIGSkORNj/BI0Yn6',
+    is_admin: false,
+    is_player: true,
+    created: '8/19/2022',
+    last_update: '9/9/2022',
+  },
+  {
+    id: 2,
+    username: 'ltyrrell1',
+    password: '$2a$04$vSpNtI2BtH9EqXj8dAdoc.4me7wswHV7DvQ4SjpC3k8v99xIT6Lvq',
+    is_admin: true,
+    is_player: false,
+    created: '5/25/2023',
+    last_update: '1/17/2023',
+  },
+  {
+    id: 3,
+    username: 'mmcguigan2',
+    password: '$2a$04$X/SK/DewVmDBSjdynh.IxuR9wAnV9MCavpokCNoN6YDu2.G5tBI5K',
+    is_admin: true,
+    is_player: false,
+    created: '11/11/2022',
+    last_update: '2/6/2023',
+  },
+  {
+    id: 4,
+    username: 'jwiskar3',
+    password: '$2a$04$zSvEL5zz3xukPIgxOjMjPu6mx.Vt35p1TBtXqgiOy2VdiGO5wbNLq',
+    is_admin: false,
+    is_player: false,
+    created: '8/13/2022',
+    last_update: '3/3/2023',
+  },
+  {
+    id: 5,
+    username: 'kbonnyson4',
+    password: '$2a$04$1HP9bLatCGKoOcjAInqz8e8Frw225t.oI1u5iPxYCffraSAjg9n.S',
+    is_admin: true,
+    is_player: false,
+    created: '5/29/2023',
+    last_update: '3/12/2023',
+  },
+  {
+    id: 6,
+    username: 'mmcilwaine5',
+    password: '$2a$04$iUgxtSgS..ShIySi6C8KL.r3RkuQibrxUVHj9LA2nXRT9pY2BvkN2',
+    is_admin: false,
+    is_player: true,
+    created: '10/6/2022',
+    last_update: '8/26/2022',
+  },
+  {
+    id: 7,
+    username: 'sgridley6',
+    password: '$2a$04$0eNAckrLMaWMqtoUTCw88.dhXAbucudDrtuhmfGsSKoBJl5jNN0ry',
+    is_admin: true,
+    is_player: false,
+    created: '4/15/2023',
+    last_update: '2/22/2023',
+  },
+  {
+    id: 8,
+    username: 'mrodrigo7',
+    password: '$2a$04$TyKlqR1d4kp9yGOk1XCx6O5JhPwGmZm0YN.nBtvwbiB6DaNRzwyum',
+    is_admin: false,
+    is_player: true,
+    created: '9/12/2022',
+    last_update: '12/8/2022',
+  },
+  {
+    id: 9,
+    username: 'agiral8',
+    password: '$2a$04$syLwppuySeNdaUNaicOnQOecyuKV05RW6hxDYVuXw4Wg6h.jMZpRO',
+    is_admin: true,
+    is_player: false,
+    created: '4/24/2023',
+    last_update: '12/5/2022',
+  },
+  {
+    id: 10,
+    username: 'mkarpushkin9',
+    password: '$2a$04$MDjVVZIasjH.HviW2jOu3.D9JSwVeFxWKLc/dbVadichvL2mG0Zs.',
+    is_admin: false,
+    is_player: false,
+    created: '11/27/2022',
+    last_update: '9/14/2022',
+  },
+];
