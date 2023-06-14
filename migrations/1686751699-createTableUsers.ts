e7fbbc109c03972f90e8304676924bfe0d7d28dd
David Ihl <daih1985@gmail.com>
2023-06-14 16:23:31 +0200

    Create tables user and players

---
+++ migrations/1686751699-createTableUsers.ts
@@ -0,0 +1,26 @@
+import { Sql } from 'postgres';
+
+export type User = {
+  id: number;
+  name: string;
+};
+
+export async function up(sql: Sql) {
+  await sql`
+    CREATE TABLE users (
+      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      username varchar(30) NOT NULL,
+      password_hash varchar(80) NOT NULL,
+      is_admin boolean NOT NULL,
+      is_player boolean NOT NULL,
+      created timestamp NOT NULL,
+      last_update timestamp NOT NULL
+    )
+  `;
+}
+
+export async function down(sql: Sql) {
+  await sql`
+    DROP TABLE users
+  `;
+}
---
+++ migrations/1686752032-createTablePlayers.ts
@@ -0,0 +1,28 @@
+import { Sql } from 'postgres';
+
+export type User = {
+  id: number;
+  name: string;
+};
+
+export async function up(sql: Sql) {
+  await sql`
+    CREATE TABLE players (
+      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      user_id integer NOT NULL,
+      alias VARCHAR(30) NOT NULL,
+      first_name VARCHAR (30),
+      last_name VARCHAR (30),
+      contact VARCHAR(100),
+      slug VARCHAR(30),
+      leagueoflegends_id integer[]
+
+    )
+  `;
+}
+
+export async function down(sql: Sql) {
+  await sql`
+    DROP TABLE players
+  `;
+}
