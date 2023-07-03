6277d4865094c100a4beb75ccee1bffce6437373
David Ihl <daih1985@gmail.com>
2023-07-03 09:37:41 +0200

    Add loading spinner

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function RootLoading() {
+  return (
+    <main className="w-full h-full flex items-center justify-center bg">
+      <span className="loading loading-spinner loading-lg bg-slate-800" />
+    </main>
+  );
+}
