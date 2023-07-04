9ea557162869c89cb68ebdbec0c2b5a9e359da13
David Ihl <daih1985@gmail.com>
2023-07-04 17:51:33 +0200

    Remove localhost for live deployment

--- app/ApolloClientProvider.tsx
+++ app/ApolloClientProvider.tsx
@@ -14,7 +14,7 @@ import {
 
 function makeClient() {
   const httpLink = new HttpLink({
-    uri: 'http://localhost:3000/api/graphql',
+    uri: 'api/graphql',
     fetchOptions: { cache: 'no-store' },
   });
 
