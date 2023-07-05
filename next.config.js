02dd5cf702172739b6c295d9656b4229ec1bce6b
David Ihl <daih1985@gmail.com>
2023-07-05 22:53:28 +0200

    Remove cache-controll in next.config.js

--- next.config.js
+++ next.config.js
@@ -10,21 +10,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // https://vercel.com/docs/concepts/edge-network/caching
-  // eslint-disable-next-line require-await
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 's-maxage=1, stale-while-revalidate=3600',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
