81778c12b443e002f87d00156ef542e9b5e5d6de
David Ihl <daih1985@gmail.com>
2023-07-05 09:24:55 +0200

    Remove unused code

--- app/_loading.tsx
+++ app/_loading.tsx
@@ -1,3 +1,5 @@
+// Left in repository but not in use due to some weird behaviour in live
+
 export default function RootLoading() {
   return (
     <main className="w-full h-full flex items-center justify-center bg">
