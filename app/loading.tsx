0c03d015a4cabfed4bc446112071a7631cefd345
David Ihl <daih1985@gmail.com>
2023-07-05 20:13:36 +0200

    Fix typo in README.md

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,3 @@
-// Left in repository but not in use due to some weird behaviour in live
-
 export default function RootLoading() {
   return (
     <main className="w-full h-full flex items-center justify-center bg">
