59807a7774db116eb00d192553125082e893c2f7
David Ihl <daih1985@gmail.com>
2023-07-06 19:15:45 +0200

    Style 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export const rootNotFoundMetadata = {
   title: 'Not Found',
   description: "sorry can't find the page you are looking for",
@@ -5,8 +7,23 @@ export const rootNotFoundMetadata = {
 
 export default function RootNotFound() {
   return (
-    <main className="p-4">
-      Sorry this page was not found make sure you visit a page that exists
+    <main className="flex flex-col items-center sm:p-4 gap-4 sm:h-full sm:justify-center">
+      <div className="shadow-xl w-full max-w-4xl bg-base-100 border-primary sm:border-t-4">
+        <div className="card-body">
+          <h1 className="my-2 text-gray-800 font-bold text-2xl">
+            Looks like you've found the doorway to the great nothing
+          </h1>
+          <p className="my-2 text-gray-800">
+            Sorry about that! Please visit our hompage to get where you need to
+            go.
+          </p>
+          <div>
+            <Link href="/" className="btn btn-primary rounded-full">
+              Take me home!
+            </Link>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
