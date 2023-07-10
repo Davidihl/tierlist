03bcf8a42e3c7acfd9e09508abd442b3eb9a280d
David Ihl <daih1985@gmail.com>
2023-07-10 15:07:34 +0200

    Style homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import headset from '../public/headset.svg';
-import mouse from '../public/mouse.svg';
+import headsetReversed from '../public/headset_reversed.svg';
 
 export default function Home() {
   return (
     <main className="h-full">
       <div className="hero min-h-full">
         <div className="hero-content">
-          <div className="max-w-md">
+          <div className="max-w-md relative">
             <h1 className="text-5xl font-bold mb-5 text-white">
               Connecting Talent. Amplifying Recognition. Shaping Esports.
             </h1>
             <Link href="/players" className="btn btn-primary rounded-full z-50">
               Browser Players
             </Link>
+            <Image
+              src={headsetReversed}
+              alt="Illustration of a headset"
+              className="absolute bottom-10 -z-10 left-10 sm:left-20 w-full sm:w-3/4"
+            />
           </div>
         </div>
       </div>
